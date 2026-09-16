cf63477ace2e0286f3d30cd6dd51827a5a6a8939
Jianjun Chen <chenjj.yz@gmail.com>
2026-09-16 21:28:09 +0800

    feat(delta-diff): accept naivediff in MCP strategy parsing (#87)

--- dbcli/src/server.rs
+++ dbcli/src/server.rs
@@ -1141,6 +1141,7 @@ fn parse_delta_diff_strategy(
         Some("bucketdiff") => Ok(Some(crate::delta_diff::cmd::Strategy::Bucketdiff)),
         Some("iblt") => Ok(Some(crate::delta_diff::cmd::Strategy::Iblt)),
         Some("keyeddiff") => Ok(Some(crate::delta_diff::cmd::Strategy::Keyeddiff)),
+        Some("naivediff") => Ok(Some(crate::delta_diff::cmd::Strategy::Naivediff)),
         Some(other) => Err(format!("unknown strategy '{other}'")),
     }
 }
@@ -1243,6 +1244,14 @@ mod delta_diff_strategy_tests {
         assert_eq!(
             parse_delta_diff_strategy(Some("keyeddiff")).unwrap(),
             Some(Strategy::Keyeddiff)
+        );
+    }
+
+    #[test]
+    fn parses_naivediff() {
+        assert_eq!(
+            parse_delta_diff_strategy(Some("naivediff")).unwrap(),
+            Some(Strategy::Naivediff)
         );
     }
 
